c06e8dfe9e0e39e4ed06c17ae64415e6e408028c
Atul Jain <atul.jain@gemsessence.com>
2018-09-20 19:25:59 +0530

    signup and login page done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  layout :select_layout
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # sign up parameters.
+    # binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :date_of_birth])
+  end
+
+  def select_layout
+    if  user_signed_in?
+      "application"
+    else
+      "before_login"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2018_09_18_112704) do
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name"
+    t.date "date_of_birth"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
